086ccf7b884f1d1d8bd358ee0412415278f9984c
mimarimbenim2026-debug <mimarimbenim2026@gmail.com>
2026-06-09 23:49:08 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,14 @@
-<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1155 1000"><path d="m577.3 0 577.4 1000H0z" fill="#fff"/></svg>
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
